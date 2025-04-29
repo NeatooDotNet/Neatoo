3911b28576ce7b2a11f5ee70ff2ec7c2857b2cee
KeithVoels <keithvoels@gmail.com>
2025-04-29 11:15:39 -0500

    Move RunRulesFlag

--- src/Neatoo/IMetaProperties.cs
+++ src/Neatoo/IMetaProperties.cs
@@ -2,19 +2,6 @@
 
 namespace Neatoo;
 
-
-[Flags]
-public enum RunRulesFlag
-{
-    None = 0,
-    NoMessages = 1,
-    Messages = 2,
-    NotExecuted = 4,
-    Executed = 8,
-    Self = 16,
-    All = NoMessages | Messages | NotExecuted | Executed | RunRulesFlag.Self
-}
-
 public interface IBaseMetaProperties
 {
     bool IsBusy { get; }
---
+++ src/Neatoo/Rules/RunRulesFlag.cs
@@ -0,0 +1,13 @@
+﻿namespace Neatoo;
+
+[Flags]
+public enum RunRulesFlag
+{
+    None = 0,
+    NoMessages = 1,
+    Messages = 2,
+    NotExecuted = 4,
+    Executed = 8,
+    Self = 16,
+    All = NoMessages | Messages | NotExecuted | Executed | RunRulesFlag.Self
+}
